ace2f51d43820a8b74c54f1ea057f54408387227
kavikushi0228 <62046302+kavikushi0228@users.noreply.github.com>
2020-03-21 16:48:54 +0530

    Add files via upload

---
+++ program01.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//NAME : KUSHANI P.G.K.M.
+//REG NO. : EG/2018/3375
+void main()
+{
+    int lower_limit,upper_limit,sum=0;
+    printf("Enter the lower limit:");
+    scanf("%d",&lower_limit);
+    printf("Enter the upper limit:");
+    scanf("%d",&upper_limit);
+    for (int i=lower_limit;i<=upper_limit;i++)
+    {
+        if(i%3 != 0)
+        {
+            sum += i;
+        }
+    }
+    printf("The sum is : %d\n",sum);
+}
+
